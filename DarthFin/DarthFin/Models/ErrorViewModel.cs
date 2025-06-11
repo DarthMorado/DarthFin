0a616c800884adf70e47c0052031c6718848a332
DarthMorado <DarthMoradoDev@gmail.com>
2025-06-12 00:43:46 +0300

    init

---
+++ DarthFin/DarthFin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DarthFin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
