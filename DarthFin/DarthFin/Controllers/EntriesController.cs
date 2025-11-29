92389f328233c66b9d70448bb64eb627c887754d
DarthMorado <DarthMoradoDev@gmail.com>
2025-11-30 00:56:53 +0200

    form

---
+++ DarthFin/DarthFin/Controllers/EntriesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DarthFin.Controllers
+{
+    public class EntriesController : Controller
+    {
+        public EntriesController()
+        {
+            
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            return View();
+        }
+    }
+}
